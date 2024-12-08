a85db9064ae3b86151d95410fb7cdcf51026d2cb
BerkTolgaSuntay123 <btolgasuntay@yahoo.com>
2024-12-08 12:43:59 -0800

    Third commit

---
+++ app/src/main/java/com/example/homework4berksuntay/ThirdFragment.kt
@@ -0,0 +1,42 @@
+package com.example.homework4berksuntay
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.homework4berksuntay.databinding.FragmentThirdBinding
+
+class ThirdFragment : Fragment() {
+
+    private var _binding: FragmentThirdBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentThirdBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Navigate to SecondFragment
+        binding.buttonThirdToSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_ThirdFragment_to_SecondFragment)
+        }
+
+        // Navigate to FirstFragment
+        binding.buttonThirdToFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_ThirdFragment_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
